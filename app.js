57e2832394f092d907f0c3cc8fb33ce18d0c2b88
BeenaAnum <beenaanum@gmail.com>
2025-06-20 20:41:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Q1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+let age = 25;
+alert(" I'm " + age + " years old ");
+
+// Q2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”
+let num = 16;
+alert(`You have visited this site ${num} times`);
+alert("You have visited this site "  + num + " times");
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+ let birthYear = 1990;
+
+ document.writeln("<h2> My Birthy Year is ", birthYear , " </h2> <br>");
+ document.writeln("<h2> Data type of my declared variable is number.</h2>");
+
+//  4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+let visitorName = "Beena Anum";
+let product = "T-shirt";
+let order = 5;
+let email = "www.WomensClothing.com";
+document.writeln("<b>", visitorName , "</b>", " ordered " , "<b>", order ," ", product , "</b>" , "(s) on" ," ", email," ", "store");
